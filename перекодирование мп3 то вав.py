7e1151928b780d438350ee19c62d62810376332d
tarasfrompir <tarasfrompir@inbox.ru>
2018-02-02 12:39:22 +0200

    Add files via upload

--- перекодирование мп3 то вав.py
+++ перекодирование мп3 то вав.py
@@ -11,11 +11,15 @@ directory = './sound/narezka'
 #Получаем список файлов в переменную files 
 files = os.listdir(directory)
 
+# dlyaskritiya processa
+CREATE_NO_WINDOW = 0x08000000
+
+
 name = 0
 
 for file in files:
     name +=1
-    subprocess.call(['ffmpeg', '-i', directory+'/'+file, './sound/wav/output'+str(name)+'.wav'])
+    subprocess.call(['ffmpeg', '-i', directory+'/'+file, './sound/wav/output'+str(name)+'.wav'], creationflags=CREATE_NO_WINDOW)
 
 
 
